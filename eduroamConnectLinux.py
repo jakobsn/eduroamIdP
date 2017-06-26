f0bc814aaf6b69f7e66c7eb5b3d188bc5998d92c
Jakob Notland <jakob_sn_@hotmail.com>
2017-06-26 14:35:27 +0200

    run main by default

--- eduroamConnectLinux.py
+++ eduroamConnectLinux.py
@@ -119,7 +119,7 @@ def wpaSupplicantConnect(identity, client_cert, ca_cert, private_key_password, p
 
 
 if __name__ == '__main__':
-    #main()
+    main()
     #getIfName()
     #networkManagerConnect("jakobsn@fyrkat.no","/home/jakobsn/uninettca/jakobsn@fyrkat.no.crt","/home/jakobsn/uninettca/FyrkatRootCA.crt","","/home/jakobsn/uninettca/jakobsn@fyrkat.no.key")
     #print(PackageExists("network-manager"))
